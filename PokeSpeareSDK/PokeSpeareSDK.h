5183676dcaac41a69ae3f0d94d3440fd4e85b616
Vinod Shabadi <vms.shabadi@gmail.com>
2021-01-15 22:16:27 +0530

    Initial Commit

---
+++ PokeSpeareSDK/PokeSpeareSDK.h
@@ -0,0 +1,19 @@
+//
+//  PokeSpeareSDK.h
+//  PokeSpeareSDK
+//
+//  Created by Vinod Shabadi on 15/01/21.
+//  Copyright © 2021 True Layer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PokeSpeareSDK.
+FOUNDATION_EXPORT double PokeSpeareSDKVersionNumber;
+
+//! Project version string for PokeSpeareSDK.
+FOUNDATION_EXPORT const unsigned char PokeSpeareSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokeSpeareSDK/PublicHeader.h>
+
+
